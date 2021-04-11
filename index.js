2a781dd95ec8c57cb1cd0e7cd305bfb2819dc3dd
Lucas Souse e Silva <sousaesilvalucas@gmail.com>
2021-04-11 16:50:04 -0300

    Added simple register user functionality.

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const axios = require('axios');
+const parse = require('csv-parse/lib/sync');
+
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const users = [];
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.post('/join', (req, res) => {
+  try {
+    const { nickname } = req.body;
+    // saving User in the 'database'
+    users.push(nickname);
+    const id = users.length;
+    res.status(201).json({ status: 'ok', result: { id, nickname } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops, an error has ocurred when registering a new user. Please, try again later',
+    });
+  }
 });
 
 io.on('connection', (socket) => {
@@ -13,11 +36,39 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+  socket.on('chat message', handleMessage);
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+// **********************************************
+async function handleMessage(message) {
+  if (message.startsWith('/stock')) {
+    const [stockCommand, stockCode] = message.split('=');
+    console.log(stockCode);
+    const stock = await getStock(stockCode);
+  } else {
+    io.emit('chat message', message);
+  }
+}
+
+async function getStock(stockCode) {
+  const options = {
+    method: 'get',
+    url: `https://stooq.com/q/l/?s=${stockCode}&f=sd2t2ohlcv&h&e=csv`,
+  };
+  const response = await axios(options);
+  const stockInfo = response.data;
+  const stock = parseStockInfo(stockInfo);
+  console.log(stock);
+}
+
+function parseStockInfo(stockInfo) {
+  const [stock] = parse(stockInfo, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+  return stock;
+}
